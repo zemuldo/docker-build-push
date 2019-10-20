0e4f3be7d191806dd1e21f599f25888bad1e16e3
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-20 23:23:41 +0300

    dummy

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,11 +32,7 @@ NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namesp
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 IMAGE_TAG=${DOCKER_IMAGE_TAG:-$GIT_TAG} ## use git ref value as docker image tag unless specified
 
-GCP_KEY=echo "$GIT_CRYPT_KEY" | base64 -d
-
 sh -c "cat "$HOME"/gcloud-service-key.json | docker login -u _json_key --password-stdin https://$REGISTRY"
-
-sh -c "docker login -u _json_key -p $GCP_KEY https://$REGISTRY"
 
 ## build the image locally
 sh -c "docker build -t $IMAGE_NAME ." ## pass in the build command from user input, otherwise build in default mode
