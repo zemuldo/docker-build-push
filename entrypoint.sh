9a2ad5ffb7000f519f2ec1361076f390746dc0ac
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-21 02:50:20 +0300

    adds push to docker hub too

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,7 +55,7 @@ if [ -n ${GCLOUD_AUTH+set} ]
   sh -c "cat "$HOME"/gcloud-service-key.json | docker login -u _json_key --password-stdin https://$REGISTRY"
 elif [ -n ${DOCKER_PASSWORD+set} ]
  then
-  sh -c "cat "$HOME"/docker-login-password.txt | docker login --username $DOCKER_USERNAME --password-stdin"
+  sh -c "docker login --username $DOCKER_USERNAME -p "$DOCKER_PASSWORD""
 else 
   echo "Not docker authorization creteria provided. Skipping login"
 fi
