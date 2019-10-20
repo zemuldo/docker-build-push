d6ed8339d89711b6eff9217ad40ec401ecd2edee
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-20 23:32:54 +0300

    working build push to gcr.io

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # mapping of var from user input or default value
 
+# Put GCP service account key from base64 to json on a file.
 echo "$2" | base64 -d > "$HOME"/gcloud-service-key.json
 
 DOCKER_REGISTRY_URL="$1"
@@ -32,13 +33,18 @@ NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namesp
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 IMAGE_TAG=${DOCKER_IMAGE_TAG:-$GIT_TAG} ## use git ref value as docker image tag unless specified
 
+# Login Docker with GCP Service Account key
+# Guide here https://cloud.google.com/container-registry/docs/advanced-authentication#gcloud_docker
 sh -c "cat "$HOME"/gcloud-service-key.json | docker login -u _json_key --password-stdin https://$REGISTRY"
 
-## build the image locally
+# Build Docker Image Locally with provided Image Name
 sh -c "docker build -t $IMAGE_NAME ." ## pass in the build command from user input, otherwise build in default mode
 
+# Set the registy URL grc.io/image_name or docker.io/image_name
 REGISTRY_IMAGE="$REGISTRY/$IMAGE_NAME"
 
+# Tag image with speciefied tag or latest
 sh -c "docker tag $IMAGE_NAME $REGISTRY_IMAGE:$DOCKER_IMAGE_TAG"
 
+# Push image to registry
 sh -c "docker push $REGISTRY_IMAGE:$IMAGE_TAG"
