1df867cf0cdfc822e9e7bafc7d5210330655f08f
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-21 04:15:33 +0300

    adds push to docker hub too

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Put GCP service account key from base64 to json on a file if specified.
-if [ -n ${GCLOUD_AUTH+set} ]
+if [ -n "${GCLOUD_AUTH+set}" ]
  then
   echo "$GCLOUD_AUTH" | base64 -d > "$HOME"/gcloud-service-key.json
-elif [ -n ${DOCKER_PASSWORD+set} ]
+elif [ -n "${DOCKER_PASSWORD+set}" ]
   then 
    echo "$DOCKER_PASSWORD" > "$HOME"/docker-login_password.text
 else
@@ -49,7 +49,7 @@ IMAGE_TAG=${DOCKER_IMAGE_TAG:-$GIT_TAG} ## use git ref value as docker image tag
 
 
 # Login Docker with GCP Service Account key or Docker username and password
-if [ -n ${GCLOUD_AUTH+set} ]
+if [ -n "${GCLOUD_AUTH+set}" ]
  then
   # Guide here https://cloud.google.com/container-registry/docs/advanced-authentication#gcloud_docker
   sh -c "cat "$HOME"/gcloud-service-key.json | docker login -u _json_key --password-stdin https://$REGISTRY"
@@ -65,12 +65,10 @@ fi
 #sh -c "docker build -t $IMAGE_NAME ." ## pass in the build command from user input, otherwise build in default mode
 
 # If Docker name name space is pecified add to registry
-if [ -n ${GCLOUD_AUTH+set} ]
+if [ -n "${GCLOUD_AUTH+set}"]
  then
-  echo "XXXXXXXXX GOOGLE CLOUD REGISTRY"
   REGISTRY_IMAGE="$REGISTRY/$IMAGE_NAME"
 else 
-  echo "XXXXXXXXX DCOKER CLOUD REGISTRY"
   REGISTRY_IMAGE="$NAMESPACE/$IMAGE_NAME"
 fi
 
